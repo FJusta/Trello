f70832b59bc5f6fece8ce1c7ca2da4e803ff1205
Justa <filjust@gmail.com>
2017-03-28 21:32:56 +0200

    first commit

---
+++ js/Board.js
@@ -0,0 +1,35 @@
+var board = {
+	name: 'Tablica Kanban',
+	createColumn: function(column) {
+	  this.element.append(column.element);
+	  initSortable();
+	},
+	element: $('#board .column-container')
+};
+
+$('.create-column')
+    .click(function() {
+        var columnName = prompt('Wpisz nazwę kolumny');
+				if (columnName) {
+        $.ajax({
+    		url: baseUrl + '/column',
+    		method: 'POST',
+    		data: {
+            	name: columnName
+    		},
+    		success: function(response){
+    			var column = new Column(response.id, columnName);
+    			board.createColumn(column);
+          	}
+        });
+			} else {
+				alert("Nazwa kolumny nie może być pusta!")
+			}
+});
+	
+function initSortable() {
+    $('.card-list').sortable({
+      connectWith: '.card-list',
+      placeholder: 'card-placeholder'
+    }).disableSelection();
+  }
